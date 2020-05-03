528a1ed573bf2189438bdf5e171a6b8ef17d9232
Crowdbotics <team@crowdbotics.com>
2020-05-03 19:55:19 +0000

    Update models

--- backend/royalty_radio_16570/wsgi.py
+++ backend/royalty_radio_16570/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'royalty_radio_16570.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "royalty_radio_16570.settings")
 
 application = get_wsgi_application()
