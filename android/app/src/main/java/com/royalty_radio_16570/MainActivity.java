fe7b2bf177f898d664c6a2e1b266e8696cfae6df
Crowdbotics <team@crowdbotics.com>
2020-05-03 19:24:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/royalty_radio_16570/MainActivity.java
@@ -0,0 +1,15 @@
+package com.royalty_radio_16570;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "royalty_radio_16570";
+    }
+}
